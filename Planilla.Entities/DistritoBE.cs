2adb7469bd9591b8d2299055c4d80300470a627e
41Presupuestos <geanfranco_lcz@hotmail.com>
2016-12-11 02:20:31 +0200

    Final de Clase
    Incompleto, Terminar en la siguiente clase

---
+++ Planilla.Entities/DistritoBE.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Planilla.Entities
+{
+    [DataContract]
+    public class DistritoBE
+    {
+        [DataMember]
+        public int IdDistrito { get; set; }
+        
+        [DataMember]
+        public string distrito { get; set; }
+        
+        [DataMember]
+        public int estado { get; set; }
+    }
+}
